6f6fee5fde1611ecb0fc48306f1c565b6912c621
bbishural <bensonbishural07@gmail.com>
2019-08-06 22:45:03 +0545

    Assignment 2: Inverse of Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,65 @@
-## Put comments here that give an overall description of what your
-## functions do
+##The first function, makeCacheMatrix creates a special "matrix", which is really a list containing a function to
 
-## Write a short comment describing this function
+## set the value of the vector
+## get the value of the vector
+## set the value of the mean
+## get the value of the mean
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  print(y)
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  
+  list(set = set,
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse
+  )
+  
 }
 
 
-## Write a short comment describing this function
+##This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+##If the inverse has already been calculated (and the matrix has not changed), 
+##then cacheSolve will retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  
+  if(!is.null(i)){
+    message("getting cached data")
+    return(i)
+  }
+  
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
+
+## To check the whether the function is correct or not
+
+##FirstMatrix <- matrix(c(1,2,3,4),2,2)
+
+##Verify <- makeCacheMatrix(FirstMatrix)
+## cacheSolve(Verify) #inverse returned after computation
+##      [,1] [,2]
+## [1,]   -2  1.5
+## [2,]    1 -0.5
+
+##cacheSolve(Verify) #inverse returned from cache
+## getting cached data
+##      [,1] [,2]
+## [1,]   -2  1.5
+## [2,]    1 -0.5
+
+
+##Result
+
